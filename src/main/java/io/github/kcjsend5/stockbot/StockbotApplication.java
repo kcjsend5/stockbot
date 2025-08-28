596c34bb3751a5c3f8eb88c595d3bf6f4595aba7
kcjsend5 <kcjsend5@naver.com>
2025-08-28 16:32:12 +0900

    [chore/#5] application.yml 생성 완료

--- src/main/java/io/github/kcjsend5/stockbot/StockbotApplication.java
+++ src/main/java/io/github/kcjsend5/stockbot/StockbotApplication.java
@@ -2,8 +2,13 @@ package io.github.kcjsend5.stockbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableFeignClients
 public class StockbotApplication {
 
 	public static void main(String[] args) {
