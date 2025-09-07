c6a1a655a1e22ff34d27e9c33bd1d249d2cb460c
kcjsend5 <kcjsend5@naver.com>
2025-09-07 21:11:55 +0900

    [feat/#17]Jwt 액세스 토큰 및 리프레쉬 토큰 생성 코드 구현

--- src/main/java/io/github/kcjsend5/stockbot/global/jwt/JwtTokenProvider.java
+++ src/main/java/io/github/kcjsend5/stockbot/global/jwt/JwtTokenProvider.java
@@ -1,18 +1,30 @@
 package io.github.kcjsend5.stockbot.global.jwt;
 
 import io.github.kcjsend5.stockbot.global.Dao.RedisDao;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+import org.springframework.security.core.Authentication;
 
 import java.security.Key;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
 import java.util.Base64;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class JwtTokenProvider {
+
+    private static final String GRANT_TYPE = "Bearer";
 
     private final Key key;
     private final UserDetailsService userDetailsService;
@@ -30,4 +42,44 @@ public class JwtTokenProvider {
         byte[] keyBytes = Base64.getEncoder().encode(secretKey.getBytes());
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
+
+    public String generateAccessToken(String email, String authorities, Date expireDate){
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("auth",authorities)
+                .setExpiration(expireDate)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken(String email,Date exprieDate){
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(exprieDate)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public JwtToken generateToken(Authentication authentication){
+
+        String authorities = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining());
+        long now = (new Date()).getTime();
+        String email = authentication.getName();
+
+        Date accessTokenExpire = new Date(now+ACCESS_TOKEN_EXPIRE_TIME);
+        String accessToken = generateAccessToken(email, authorities, accessTokenExpire);
+
+        Date refreshTokenExpire = new Date(now+REFRESH_TOKEN_EXPIRE_TIME);
+        String refreshToken = generateRefreshToken(email, refreshTokenExpire);
+
+        redisDao.setValues(email,refreshToken, Duration.ofMillis(REFRESH_TOKEN_EXPIRE_TIME));
+
+        return JwtToken.builder()
+                .grantType(GRANT_TYPE)
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+
+    }
+
 }
